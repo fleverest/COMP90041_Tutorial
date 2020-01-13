061eb8df3ae89d445cced2e064c2d5468ac270a2
Floyd <feverestpersonal@gmail.com>
2020-01-13 18:36:19 +1100

    first solution for HW1

---
+++ tutorial 1/mysolutions/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String myName = "Floyd Everest";
+        String myPhone = "0412-363-374";
+        String myAddress = "290 Hope St";
+
+        System.out.println(myName);
+        System.out.println(myPhone);
+        System.out.println(myAddress);
+    }
+}
